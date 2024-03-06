bbefc4ec0181289d260986fe2b8f982cd5d2466d
Vittutexx <vitortex06@gmail.com>
2024-03-06 17:41:34 -0300

    comentando a classe ContaBancaria

---
+++ src/Exericios02/src/ContaBancaria.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ContaBancaria {
+    /*
+    //Exercicio 1
+    private int numeroConta;
+    private double saldo;
+    public String titular;
+
+        //GETERS E SETTERS
+        public int getNumeroConta() {
+            return numeroConta;
+        }
+
+        public void setNumeroConta(int numeroConta) {
+            this.numeroConta = numeroConta;
+        }
+
+            public double getSaldo() {
+            return saldo;
+        }
+
+        public void setSaldo(double saldo) {
+            this.saldo = saldo;
+        }
+    }
+    */
+    }
