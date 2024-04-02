d71d93cc76cd561c7e452a0a19b787c8489369bb
Vitor Teixeira Silva <vitortex06@gmail.com>
2024-04-02 16:10:50 -0300

    Utilizando a interface Compare para implementar na classe titulo e comparar entre os nomes de cada titulo para utilizar o método CompareTo() na arraylist de Titulo na classe principal .

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -3,6 +3,7 @@ import br.com.alura.screenmatch.modelo.Filme;
 import br.com.alura.screenmatch.modelo.Serie;
 import br.com.alura.screenmatch.modelo.Titulo;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalComListas {
 
@@ -31,5 +32,19 @@ public class PrincipalComListas {
 
             }//if
         }//for
+
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jaqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+
+        System.out.println("\nDepois da ordenação do colletions: \n" + buscaPorArtista);
+        System.out.println("\nLista de titulos ordenados: \n" + lista);
+        Collections.sort(lista);
+
     }//main
 }//class
